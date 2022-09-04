35f3df847d1224e332470a6a2a48b999103c3416
Nigel Patching <75135800+nigels-code@users.noreply.github.com>
2022-09-04 21:53:58 +0100

    chore: more stuff

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    document.title = "The Chatty Potter";
+  });
   return (
     <div className="container">
       <h1 className="center">The Chatty Potter</h1>

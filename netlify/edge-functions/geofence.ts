00515a31c6496c7cf85f74308404e8aea7067ead
Nigel Patching <75135800+nigels-code@users.noreply.github.com>
2022-09-11 17:49:48 +0100

    chore: stuff

--- netlify/edge-functions/geofence.ts
+++ netlify/edge-functions/geofence.ts
@@ -1,17 +1,17 @@
-import { Context } from "netlify:edge";
+// import { Context } from "netlify:edge";
 
-export default (_request: Request, context: Context) => {
-  const GEOFENCE = ["GB", "FR"];
-  const countryCode = context.geo?.country?.code || "GB";
-  const countryName = context.geo?.country?.name || "Outside the UK";
+// export default (_request: Request, context: Context) => {
+//   const GEOFENCE = ["GB", "FR"];
+//   const countryCode = context.geo?.country?.code || "GB";
+//   const countryName = context.geo?.country?.name || "Outside the UK";
 
-  if (!GEOFENCE.includes(countryCode)) {
-    return new Response(
-      `We're sorry, you can't access our content from ${countryName}`,
-      {
-        headers: { "content-type": "text/html" },
-        status: 418,
-      }
-    );
-  }
-};
+//   if (!GEOFENCE.includes(countryCode)) {
+//     return new Response(
+//       `We're sorry, you can't access our content from ${countryName}`,
+//       {
+//         headers: { "content-type": "text/html" },
+//         status: 418,
+//       }
+//     );
+//   }
+// };
